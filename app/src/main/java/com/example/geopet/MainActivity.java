def58c2f4b82de248bec5ed97a333e06c620edd6
dtorresg <daniel.torresg@sansano.usm.cl>
2021-06-14 18:09:27 -0400

    Dashboard

--- app/src/main/java/com/example/geopet/MainActivity.java
+++ app/src/main/java/com/example/geopet/MainActivity.java
@@ -3,30 +3,67 @@ package com.example.geopet;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-//hola
-public class MainActivity extends AppCompatActivity {
 
+import Model.Post;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-
-
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        setContentView(R.layout.listview_layout);
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayList<Card> list = new ArrayList<>();
+        db.collection("post").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        System.out.println("HOLAPOTOPOTOPOTO");
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                list.add(new Card("drawable://" + R.drawable.pic, (String) document.getData().get("descripcion")));
+                            }
+                            CustomListAdapter adapter = new CustomListAdapter(MainActivity.this,R.layout.activity_main,list);
+                            mListView.setAdapter(adapter);
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        CustomListAdapter adapter = new CustomListAdapter(MainActivity.this,R.layout.activity_main,list);
+        mListView.setAdapter(adapter);
     }
 
     public void logout(View view) {
